accd5082d40f641c8597c489ac3e19ac9383c423
Evan Schoening <evan.schoening@gmail.com>
2023-08-01 08:53:55 -0700

    initial commit of cli tool

---
+++ echor/src/main.rs
@@ -0,0 +1,11 @@
+use clap::{arg, Command};
+
+
+fn main() {
+    println!("{:?}", std::env::args());
+    let matches = Command::new("echor")
+        .version("1.0")
+        .author("Evan S")
+        .about("simple Rust implementation of echo")
+        .get_matches();
+}
