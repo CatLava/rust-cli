5af29a75c306f82e9be124241057ed5bf3c3df5e
Evan Schoening <evan.schoening@gmail.com>
2023-08-09 20:07:48 -0700

    working arg parser

--- catter/src/lib.rs
+++ catter/src/lib.rs
@@ -11,29 +11,42 @@ pub struct Config {
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn get_args() -> MyResult<Config> {
-    arg = Command::new("catter")
+    let items = Command::new("catter")
         .version("1.0")
         .author("Evan S")
         .about("Rust implementation of cat")
         .arg(
-            .Arg::new("files")
+            Arg::new("files")
                 .help("list of files to cat")
+                .short('f')
                 .required(true)
                 .num_args(1..)
-                .action(ArgAction::Set)
+                .value_parser(value_parser!(String))
+                .action(ArgAction::Append)
         )
         .arg(
-            .Arg::new("number_lines")
+            Arg::new("number_lines")
                 .help("number the lines of catted file; bool")
-                .short("-nl")
-                .required(true)
+                .short('n')
+                .required(false)
         )
         .arg(
-            .Arg::new("number_nonblank_lines")
+            Arg::new("number_nonblank_lines")
                 .help("count blank lines; bool")
-                .short("-b")
-                .required(true)
+                .short('b')
+                .required(false)
         )
+        .get_matches();
+    println!("{:#?}", items);
+
+    Ok(Config {
+        files: items.get_many("files")
+                        .unwrap()
+                        .map(|a: &String| a.to_string())
+                        .collect(),
+        number_lines: false,
+        number_nonblank_lines: false
+    })
 
 }
 pub fn run() -> MyResult<()> {
